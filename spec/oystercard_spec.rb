34a2e62d01b0b10aa46151dc58c6b97bb27b9fa7
Colin McCulloch <mccolane@gmail.com>
2015-12-01 16:01:51 +0000

    Pt.6 Add Maximum Balance REFACTOR

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,20 +1,21 @@
 require 'oystercard'
 describe Oystercard do
   #oystercard = Oystercard.new
-#
+#let(:oystercard) { double :oystercard }
+  let(:oystercard) { described_class.new }
    describe '#balance' do
      it 'returns balance as 0' do
-       expect(subject.balance).to eq (0)
+       expect(oystercard.balance).to eq (0)
      end
    end
  describe '#top_up' do
      it { is_expected.to respond_to(:top_up).with(1).argument }
     it 'adds cash to the oystercard' do
-      expect { subject.top_up(10) }.to change{ subject.balance }.by 10
+      expect { oystercard.top_up(10) }.to change{ oystercard.balance }.by 10
     end
     it 'prevent balance from exceeding £90' do
-      subject.top_up(90)
-      expect {subject.top_up(1)}.to raise_error "Balance cannot exceed £#{Oystercard::LIMIT}"
+      oystercard.top_up(90)
+      expect {oystercard.top_up(1)}.to raise_error "Balance cannot exceed £#{Oystercard::LIMIT}"
     end
  end
 
