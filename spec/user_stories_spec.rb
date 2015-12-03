6813f2a0f1e8ac6df67f30ec6e6c4ffae8a64f14
Hamid Zade <hamidmamadzade@gmail.com>
2015-12-03 17:11:41 +0000

    challenge 13 completed

--- spec/user_stories_spec.rb
+++ spec/user_stories_spec.rb
@@ -7,32 +7,6 @@
 require 'oystercard'
 
 describe 'User Stories' do
-  oystercard = Oystercard.new
-  it 'responds to balance' do
-  expect { oystercard.balance }.not_to raise_error
-  end
-
-  oystercard = Oystercard.new
-  it 'adds money to oystercard' do
-  expect { oystercard.top_up(Oystercard::MINIMUM) }.not_to raise_error
-  end
-
-  it 'touching in' do
-  oystercard = Oystercard.new
-  oystercard.top_up(Oystercard::MINIMUM) 
-    expect { oystercard.touch_in('entry_station') }.not_to raise_error
-  end
-
-  it 'touching out' do
-  oystercard = Oystercard.new 
-    expect { oystercard.touch_out('exit_station') }.not_to raise_error
-  end
-
-  it 'in journey' do
-    oystercard = Oystercard.new
-    expect { oystercard.in_journey?}.not_to raise_error
-  end
-
   it 'raise error if balance is less than minimum fare' do 
     oystercard = Oystercard.new
     expect { oystercard.touch_in('entry_station') }.to raise_error 'Not enough funds available'
